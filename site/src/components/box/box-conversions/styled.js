6112fa3605311f41bc135452421b795c75a1d1a1
João Pedro <joaopedrosilvadossantos2003@gmail.com>
2022-01-17 18:52:08 -0300

    jk

--- site/src/components/box/box-conversions/styled.js
+++ site/src/components/box/box-conversions/styled.js
@@ -43,69 +43,105 @@ input{
 export default function BoxConversions(props){
     
     const [con, setCon] = useState({})
-    const [teste, setTeste] = useState(0)
+    const [Num1, setNum1] = useState('')
+    const [Option1, setOption1] = useState('')
+    const [Option2, setOption2] = useState('')
 
-    async function Conversão() {
+     async function Conversão() {
+         
+                if( Option1 === 'op2' && Option2 === 'ep1' && props.op2 === 'Real Brasileiro' && props.ep1 === 'Dolár Americano') {
+                let resp = await axios.get(`https://economia.awesomeapi.com.br/last/BRL-USD`);
+                setCon( [resp.data.BRLUSD.ask] * Num1)
+              
+                }
+                else if (Option1 === 'op1' && Option2 === 'ep2' && props.op1 === 'Dolár Americano' && props.ep2 === 'Real Brasileiro') {
+                    let resp = await axios.get(`https://economia.awesomeapi.com.br/last/USD-BRL`);
+                    setCon([resp.data.USDBRL.ask] * Num1)
+                    console.log(setCon)
+                }
 
-        if( teste === 2) {
-        let resp = await axios.get(`https://economia.awesomeapi.com.br/last/BRL-USD`);
-       setCon([resp.data.BRLUSD.ask])    
-        console.log(setTeste)
-        }
-        else {
-            setCon(0)
-        }
+                else if(Option1 === 'op4' && Option2 === 'ep2' && props.op4 === 'Euro' && props.ep2 === 'Real Brasileiro') {
+                    let resp = await axios.get(`https://economia.awesomeapi.com.br/last/EUR-BRL`);
+                    setCon([resp.data.EURBRL.ask] * Num1)
+                    console.log(setCon)
+                }
 
+                else if(Option1 === 'op2' && Option2 === 'ep4' && props.op2 === 'Real Brasileiro' && props.ep4 === 'Euro') {
+                    let resp = await axios.get(`https://economia.awesomeapi.com.br/last/BRL-EUR`);
+                    setCon([resp.data.BRLEUR.ask] * Num1)
+                    console.log(setCon)
+
+                }
+
+                
+
+                else if(Option1 === 'op5' && Option2 === 'ep2' && props.op5 === 'Guarani Paraguaio' && props.ep2 === 'Real Brasileiro') {
+                    let resp = await axios.get(`https://economia.awesomeapi.com.br/last/PYG-BRL`);
+                    setCon([resp.data.PYGBRL.ask] * Num1)
+                    console.log(setCon)
+                    
+                }
+
+                else if(Option1 === 'op2' && Option2 === 'ep5' && props.op2 === 'Real Brasileiro' && props.ep5 ===  'Guarani Paraguaio'  ) {
+                    let resp = await axios.get(`https://economia.awesomeapi.com.br/last/BRL-PYG`);
+                    setCon([resp.data.BRLPYG.ask] * Num1)
+                    console.log(setCon)
+                    
+                }
+
+                else {
+                    setCon(Num1)
+                }
         
+       
+
     };
 
+    console.log(Option2)  
   useEffect(() => {
-        Conversão();
       
-    }, [])  
+      Conversão();
+    })  
 
     return(
         <ContainerBoxConversions>
             <div className="title">{props.titulo}</div>
             <div className="sub-title"> Quero Converter </div>
             <div className="inputs">
-                <input />
+                <input value={Num1} onChange={(e) => setNum1(Number(e.target.value))}/>
             </div>
             <div className="inputs">
-                <select  value={teste} onChange={Number => setTeste(Number.target.value)} > 
-                    <option value="1">{props.op1}</option>
-                    <option value="2"> {props.op2}</option>
-                    <option value="3">{props.op3}</option>
-                    <option value="4">{props.op4}</option>
-                    <option style={props.op5 === '' ? {display: 'none'} : {display: 'block'}}>{props.op5}</option>
-                    <option style={props.op6 === '' ? {display: 'none'} : {display: 'block'}}>{props.op6}</option>
-                    <option style={props.op7 === '' ? {display: 'none'} : {display: 'block'}}>{props.op7}</option>
-                    <option style={props.op8 === '' ? {display: 'none'} : {display: 'block'}}>{props.op8}</option>
-                    <option style={props.op9 === '' ? {display: 'none'} : {display: 'block'}}>{props.op9}</option>
-                    <option style={props.op10 === '' ? {display: 'none'} : {display: 'block'}}>{props.op10}</option>
-                    
+                <select  value={Option1} onChange={e => setOption1(e.target.value)} > 
+                    <option value="op1" >{props.op1} </option>
+                    <option value="op2"> {props.op2} </option>
+                    <option value="op3"> {props.op3}  </option>
+                    <option value="op4"> {props.op4}  </option>
+                    <option value="op5" style={props.op5 === '' ? {display: 'none'} : {display: 'block'}} >{props.op5}</option>
+                    <option value="op6" style={props.op6 === '' ? {display: 'none'} : {display: 'block'}}>{props.op6}</option>
+                    <option value="op7" style={props.op7 === '' ? {display: 'none'} : {display: 'block'}}>{props.op7}</option>
+                    <option value="op8" style={props.op8 === '' ? {display: 'none'} : {display: 'block'}}>{props.op8}</option>
+                    <option value="op9" style={props.op9 === '' ? {display: 'none'} : {display: 'block'}}>{props.op9}</option>
+                    <option value="op10" style={props.op10 === '' ? {display: 'none'} : {display: 'block'}}>{props.op10}</option>
                 </select>
             </div>
             <div className="sub-title">Para</div>
             <div className="inputs">
-                <select>
-                    <option id="um1" >{props.ep1}</option>
-                    <option>{props.ep2}</option>
-                    <option>{props.ep3}</option>
-                    <option>{props.ep4}</option>
-                    <option style={props.ep5 === '' ? {display: 'none'} : {display: 'block'}} >{props.ep5}</option>
-                    <option style={props.ep6 === '' ? {display: 'none'} : {display: 'block'}}>{props.ep6}</option>
-                    <option style={props.ep7 === '' ? {display: 'none'} : {display: 'block'}}>{props.ep7}</option>
-                    <option style={props.ep8 === '' ? {display: 'none'} : {display: 'block'}}>{props.ep8}</option>
-                    <option style={props.ep9 === '' ? {display: 'none'} : {display: 'block'}}>{props.ep9}</option>
-                    <option style={props.ep10 === '' ? {display: 'none'} : {display: 'block'}}>{props.ep10}</option>
+                <select value={Option2} onChange={e => setOption2(e.target.value)}>
+                    <option value="ep1" >{props.ep1}</option>
+                    <option  value="ep2">{props.ep2}</option>
+                    <option  value="ep3">{props.ep3}</option>
+                    <option  value="ep4">{props.ep4}</option>
+                    <option  value="ep5" style={props.ep5 === '' ? {display: 'none'} : {display: 'block'}} >{props.ep5}</option>
+                    <option   value="ep6" style={props.ep6 === '' ? {display: 'none'} : {display: 'block'}}>{props.ep6}</option>
+                    <option  value="ep7" style={props.ep7 === '' ? {display: 'none'} : {display: 'block'}}>{props.ep7}</option>
+                    <option  value="ep8" style={props.ep8 === '' ? {display: 'none'} : {display: 'block'}}>{props.ep8}</option>
+                    <option  value="ep9" style={props.ep9 === '' ? {display: 'none'} : {display: 'block'}}>{props.ep9}</option>
+                    <option   value="ep10"style={props.ep10 === '' ? {display: 'none'} : {display: 'block'}}>{props.ep10}</option>
                 </select>
             </div>
             <div className="inputs">
-                
-             
                 <input value={con} />
-                </div>
+            </div>
         </ContainerBoxConversions>
     )
 }
